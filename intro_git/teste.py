b7d54489dcb95bdcf555c7b09f92ec5e3acc8c82
B. Yumi <yumisekii@gmail.com>
2026-08-13 21:50:44 -0300

    test(ap01-git): testa o menu

--- intro_git/teste.py
+++ intro_git/teste.py
@@ -1,7 +1,10 @@
 import calculadora
+import menu
 a = 5
 b = 8
 print(f'{a} + {b} = {calculadora.somar(a, b)}')
 print(f'{a} - {b} = {calculadora.subtrair(a, b)}')
 print(f'{a} * {b} = {calculadora.multiplicar(a, b)}')
 print(f'{a} / {b} = {calculadora.dividir(a, b)}')
+
+menu.menu
