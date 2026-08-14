3ae52de5c5db27f346026c3c2e1e251708a15e65
B. Yumi <yumisekii@gmail.com>
2026-08-13 21:37:48 -0300

    test(ap01-git): testa a divisao

--- intro_git/teste.py
+++ intro_git/teste.py
@@ -4,3 +4,4 @@ b = 8
 print(f'{a} + {b} = {calculadora.somar(a, b)}')
 print(f'{a} - {b} = {calculadora.subtrair(a, b)}')
 print(f'{a} * {b} = {calculadora.multiplicar(a, b)}')
+print(f'{a} / {b} = {calculadora.dividir(a, b)}')
